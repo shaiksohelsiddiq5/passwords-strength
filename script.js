c4073ef0ab6cd4a93a4f69936c0a02ee6d87ca74
siddiq <shaiksohelsiddiq5@gmail.com>
2026-05-25 22:21:55 +0530

    Updated password checker

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function checkPassword() {
     let point = 0;
     let value = password.value;
 
-    let widthpower = ["1%", "25%", "50%", "75%", "100%"];
+    let widthpower = ["0%", "25%", "50%", "75%", "100%"];
     let color = ["red", "orange", "yellow", "lightgreen", "green"];
 
-    if (value.length >= 6) {
+    if (value.length >= 6){
         let arrayTest = [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/];
 
         arrayTest.forEach((item) => {
-            if (item.test(value)) {
+            if (item.test(value)){
                 point += 1;
             }
         });
     }
 
-    power.style.width = widthpower[point] || "0%";
-    power.style.backgroundColor = color[point] || "red";
+    power.style.width = widthpower[point];
+    power.style.backgroundColor = color[point];
 }
